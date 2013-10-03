8b540ce37ecb3ed23cd04a306191b4e1f3a654eb
denizozger <deniz@parallelbrains.com>
2013-10-03 11:57:14 -0600

    Added the missing class

---
+++ src/main/java/com/parallelbrains/baselayout/security/UserAuthenticationManager.java
@@ -0,0 +1,34 @@
+package com.parallelbrains.baselayout.security;
+
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO not used at the moment, will remove after all authentication work is done - if it's still not used
+ */
+public class UserAuthenticationManager implements AuthenticationManager {
+
+    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+
+    static {
+        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+        if (auth.getName().equals(auth.getCredentials())) {
+            return new UsernamePasswordAuthenticationToken(auth.getName(),
+                    auth.getCredentials(), AUTHORITIES);
+        }
+        throw new BadCredentialsException("Bad Credentials");
+    }
+
+}
